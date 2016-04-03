fdfa374b139f897da9a9b29b95bcfdb40929bd52
YUGENG CHANG <chang583778395@gmail.com>
2016-04-02 22:33:15 -0700

    final

--- CS526(A)_QUIZ3_9790_YUGENG CHANG/models/calculator.php
+++ CS526(A)_QUIZ3_9790_YUGENG CHANG/models/calculator.php
@@ -2,27 +2,40 @@
 
 class Calculator {
 
-    private $result;
-    private $enter;
-    public $lista = array(
-        "Alabama" => Montgomery,
-        "Montana" => Helena,
-        "Alaska" => Juneau,
-        "Arizona" => Phoenix,
+    public $lista = array( 
+        "Alabama" => "Montgomery",
+            "Montana" => "Helena",
+            "Alaska" => "Juneau",
+            "Arizona" => "Phoenix"
     );
 
  
 
-    public function __construct($enter) {
-        $this->enter = $enter;
+    public function __construct() {
         $this->lista = array(
-            "Alabama" => Montgomery,
-            "Montana" => Helena,
-            "Alaska" => Juneau,
-            "Arizona" => Phoenix,
+            "Alabama" => "Montgomery",
+            "Montana" => "Helena",
+            "Alaska" => "Juneau",
+            "Arizona" => "Phoenix"
         );
     }
        public function shuffle_assoc() {
         shuffle($this->lista);
+    }
+    public function check($key,$answer)
+    {   
+       var_dump($answer);
+        var_dump( $this->lista[$key]);
+        if ($this->lista[$key]==$answer)
+        {
+            echo '1';
+            return '1';
+        }
+        else
+        {   
+            echo '0';
+            return '0';
+            
+        }
     }
 }
