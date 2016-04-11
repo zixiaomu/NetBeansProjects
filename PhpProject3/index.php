bd1e2876eedf59c2e6e5e6c4f8568fb38b121323
zixiaomu <chang583778395@gmail.com>
2016-04-10 18:27:44 -0700

    quiz4

---
+++ PhpProject3/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+
+<!-- 
+
+
+Student Info: Name=YUGENG CHANG, ID=9790
+
+Subject: CS526(A)_HWNo_SPRING_2016
+
+Author: yugengchang 
+
+Filename: index.php 
+
+Date and Time: Apr 8, 2016 6:21:58 PM 
+
+Project Name: yugengchang 
+
+
+--> 
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
